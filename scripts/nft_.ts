0c8ee8cb05c54dbc47ad8dbec262b08aad951ff8
Evans007 <ebounce500@gmail.com>
2023-03-12 09:12:06 +0100

    contract verified... success

--- scripts/nft_.ts
+++ scripts/nft_.ts
@@ -8,18 +8,7 @@ async function main() {
   await marketplace.deployed()
   const marketplaceAddress = marketplace.address;
 
-  console.log(`NFT market deployed to: ${marketplaceAddress}`);
-
-  // 0xf3cbCFE00a258d1A8F3950dfB353111D3F65d031
-
-  ////////////////  NFT TOKEN  /////////////////
-  const NFT = await ethers.getContractFactory("NFT");
-  const Nft = await NFT.deploy(marketplaceAddress);
-  await Nft.deployed();
-  const NftAddr = Nft.address;
-  console.log(`Nft deployed to: ${NftAddr}`);
-  
-  // 0xae8c16c60ED69ba3528D8a4B745568938ae0629b
+  console.log(marketplaceAddress);
   
 
 }
